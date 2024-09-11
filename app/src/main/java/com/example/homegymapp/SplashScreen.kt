c393e4445e4a0131bf194b9104309a4a97c08f03
WahidMalik <malikwahi705@gmail.com>
2024-09-11 21:31:37 +0500

    Change

--- app/src/main/java/com/example/homegymapp/SplashScreen.kt
+++ app/src/main/java/com/example/homegymapp/SplashScreen.kt
@@ -1,18 +1,27 @@
 package com.example.homegymapp
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.util.logging.Handler
+import com.example.homegymapp.databinding.ActivitySplashScreenBinding
+
 
 class SplashScreen : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val slideUp: Animation = AnimationUtils.loadAnimation(this,R.anim.slide_in_left)
+        binding.icon.startAnimation(slideUp)
+        binding.title.startAnimation(slideUp)
+        binding.description.startAnimation(slideUp)
 
         android.os.Handler().postDelayed({
             val intent = Intent(this@SplashScreen, MainActivity::class.java)

17d1474e62128e9afcb0826524dfac0036ba7e3d
WahidMalik <malikwahi705@gmail.com>
2024-09-12 21:55:46 +0500

    Change

--- app/src/main/java/com/example/homegymapp/MainActivity.kt
+++ app/src/main/java/com/example/homegymapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.homegymapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.homegymapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
